6f0759755693ffa219760221961f001b8c221951
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2023-02-24 12:55:46 -0300

    Configure CartService to get shipping prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class CartService {
   items: Product[] = [];
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   addToCart(product: Product){
     this.items.push(product);
@@ -21,5 +21,9 @@ export class CartService {
   clearCart(){
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices(){
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
